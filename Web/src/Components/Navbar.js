2a6c735dc19a59383d1e61e7d5568d4ff1fd0075
Iyushe <aayushitandale-ecs@atharvacoe.ac.in>
2023-02-24 14:24:08 +0530

    added frontend

---
+++ Web/src/Components/Navbar.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import {  Link } from "react-router-dom";
+const Navbar= () =>{
+    return (
+    <div>
+      <li>
+        <Link to="/">Home</Link>
+      </li>
+      <li>
+        <Link to="/main">Main</Link>
+      </li>
+      <li>
+        <Link to="/login">Login</Link>
+      </li>
+    
+    </div>
+    );
+  }
+  export default Navbar;
