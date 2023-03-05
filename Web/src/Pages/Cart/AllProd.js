cdba323043606574352950cb0605d9e402c63a99
Sahil8383 <95122593+Sahil8383@users.noreply.github.com>
2023-03-05 15:23:29 +0530

    Cart Css

--- Web/src/Pages/Cart/AllProd.js
+++ Web/src/Pages/Cart/AllProd.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import SingleProd from './SingleProd'
-
+import './cart.css'
 function AllProd() {
 
   const [data, setData] = React.useState([])
@@ -13,7 +13,24 @@ function AllProd() {
 
   return (
     <>
-      <SingleProd Name={"Item1"} Price="500" Img={''}/>
+      <h1 className='title' >My Cart</h1>
+
+      <div className='grid' >
+
+        {/* Example ke liye hardcode kiya hai */}
+
+        {/* <SingleProd Name={"Item1"} Price="500" Img={''} />
+        <SingleProd Name={"Item1"} Price="500" Img={''} />
+        <SingleProd Name={"Item1"} Price="500" Img={''} />
+        <SingleProd Name={"Item1"} Price="500" Img={''} /> */}
+        {
+          data.map((item) => {
+            return (
+              <SingleProd Name={item.Name} Price={item.Price} Img={item.Img} />
+            )
+          })
+        }
+      </div>
     </>
   )
 }
