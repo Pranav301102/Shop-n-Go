dc6e6bbae4ce46a4357961b544eaeb95fda0dab2
shubham dhumal <94701914+shubh042@users.noreply.github.com>
2023-03-18 22:04:50 +0530

    Update index.js

--- Web/src/Pages/Signin/index.js
+++ Web/src/Pages/Signin/index.js
@@ -107,7 +107,7 @@ function Overlay() {
           </BgImg>
         </Left>
         <Right>
-          <h2>Sign UP</h2>
+          <h2>Sign Up</h2>
           <Form>
             <form>
                 <>
@@ -144,7 +144,7 @@ function Overlay() {
               
                 <p>
                   Don't have an account?
-                  <Btn onClick={() =>navigate('/signup')}> Sign UP </Btn>{" "}
+                  <Btn onClick={() =>navigate('/signup')}> Sign Up </Btn>{" "}
                 </p>
               
               
